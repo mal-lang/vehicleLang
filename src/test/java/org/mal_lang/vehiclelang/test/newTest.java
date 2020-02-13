a8a0b56ed56fe2fb24f91e6441e63efd59be9fb4
Sotirios Katsikeas <skatsikeas@outlook.com>
2020-02-13 18:52:34 +0100

    Completely changed the way a MessageID can be exploited to gain manipulate on a sensor/actuator (now it happens via Dataflow, which has a MessageID connected) Additionally, made some improvements on VehicleNetwork.messageInjection and ECU.offline

--- src/test/java/org/mal_lang/vehiclelang/test/newTest.java
+++ src/test/java/org/mal_lang/vehiclelang/test/newTest.java
@@ -13,66 +13,51 @@ public class newTest {
    public void acceleratorTest() {
       // This test case was created in the reviewing session with another domain specific language developer
       // Update 2018-12-05: Changed to assert the network as uncompromised because of modification to maliciousFirmwareModification parents
+      // Update 2020-02-13: Changed almost all the model to make use of the new and imrpoved way to exploit known ID on bus networks.
       /*
-                                ---------------------------------   
-                                |                               |
-       Transmitter <---> accelDataflow   ---> Firmware          |
-              |            |             |                      |
-          accelECU <---> vNet1 <---> GatewayECU <---> vNet2     |
-                           |             |                      |
-          engineECU <-------            IDPS                    |
-           |    |                                               |
-        engine  -----> acceleratorAccount <---> canID <----------
+                                -----> canID  
+                                |                                 
+       Transmitter <---> accelDataflow       
+              |            |                                 
+          accelECU <---> vNet1
+                           |                     
+          engineECU <-------                 
+           |                                                   
+        engine  
             
       */
       System.out.println("### " + Thread.currentThread().getStackTrace()[1].getMethodName());
       // Start of test
-      boolean firewallStatus = true;
-      boolean firmwareValidationStatus = false;
-      boolean secureBootStatus = false;
       ECU acceleratorEcu = new ECU ("acceleratorEcu", true, true); // Enabled operation mode and message confliction protection on all ECUs.
       ECU engineEcu = new ECU ("engineEcu", true, true);
-      GatewayECU gateEcu = new GatewayECU ("GatewayECU", firewallStatus, true, true);
-      IDPS idps = new IDPS ("IDPS");
       VehicleNetwork vNet1 = new VehicleNetwork ("vNet1");
-      VehicleNetwork vNet2 = new VehicleNetwork ("vNet2");
-      ConnectionlessDataflow accelarationDataflow = new ConnectionlessDataflow("accelarationDataflow");
+      ConnectionlessDataflow accelerationDataflow = new ConnectionlessDataflow("accelerationDataflow");
       TransmitterService transmitter = new TransmitterService("Transmitter");
-      Firmware fw = new Firmware ("fw", firmwareValidationStatus, secureBootStatus);
+      VehicleNetworkReceiver receiver = new VehicleNetworkReceiver("Receiver");
       SensorOrActuator engine = new SensorOrActuator ("engine");
       MessageID canID = new MessageID ("CAN-ID");
-      Account acceleratorAccount = new Account ("acceleratorAccount");
 
       acceleratorEcu.addExecutees(transmitter);
-      transmitter.addDataflows(accelarationDataflow);
+      transmitter.addDataflows(accelerationDataflow);
       acceleratorEcu.addVehiclenetworks(vNet1);
       engineEcu.addVehiclenetworks(vNet1);
       engineEcu.addSensorsOrActuators(engine);
-      vNet1.addDataflows(accelarationDataflow);
-      gateEcu.addTrafficVNetworks(vNet1);
-      gateEcu.addTrafficVNetworks(vNet2);
-      gateEcu.addIdps(idps);
-      gateEcu.addFirmware(fw);
+      engineEcu.addExecutees(receiver);
+      receiver.addDataflows(accelerationDataflow);
+      vNet1.addDataflows(accelerationDataflow);
       
-      acceleratorAccount.addCredentials(canID);
-      accelarationDataflow.addData(canID);
-      engineEcu.addAccount(acceleratorAccount);
+      accelerationDataflow.addDataflowId(canID);
       
       Attacker atk = new Attacker();
-      atk.addAttackPoint(vNet2.physicalAccess);
-      atk.addAttackPoint(canID.read);
+      atk.addAttackPoint(vNet1.physicalAccess);
       atk.attack();
       
-      vNet2.physicalAccess.assertCompromisedInstantaneously();
-      vNet2.accessNetworkLayer.assertCompromisedInstantaneously();
-      gateEcu.connect.assertCompromisedInstantaneously();
-      fw.maliciousFirmwareModification.assertUncompromised();
-      vNet1.accessNetworkLayer.assertUncompromised();
-      accelarationDataflow.transmit.assertUncompromised();
-      accelarationDataflow.eavesdrop.assertUncompromised();
+      vNet1.accessNetworkLayer.assertCompromisedInstantaneously();
+      vNet1.eavesdrop.assertCompromisedInstantaneously();
+      accelerationDataflow.eavesdrop.assertCompromisedInstantaneously();
       
-      //engine.access.assertUncompromised();
-      acceleratorAccount.idAuthenticate.assertCompromisedInstantaneously();
+      engineEcu.access.assertUncompromised();
+      accelerationDataflow.eavesdropId.assertCompromisedInstantaneously();
       engineEcu.idAccess.assertCompromisedInstantaneously();
       engine.manipulate.assertCompromisedInstantaneously();
     }
